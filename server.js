fd6a0b6b884e30265cf7a9b89b590ad99287a9b5
Sanjana <sanjana.sureshkumar05@gmail.com>
2025-11-07 09:57:02 +0530

    comments

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const cors = require('cors'); //connect frontend to backend
 
-const app = express();
+const app = express(); //start express backend instance
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(cors()); //allows 3000 to 5000
+app.use(express.json()); //json to javascript conversion
+app.use(express.urlencoded({ extended: true })); //when fronted gives html instead of json
+ 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/terraceclub')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth')); //use routes defined in /routes/auth
 app.use('/api/plants', require('./routes/plants'));
 app.use('/api/posts', require('./routes/posts'));
 
